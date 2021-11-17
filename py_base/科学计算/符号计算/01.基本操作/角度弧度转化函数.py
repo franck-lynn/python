92cb11b3f4551f2e67a66d31b1e68a25f536dd6a
franck-lynn <lry_demry@163.com>
2021-11-17 17:32:09 +0800

    py

---
+++ py_base/科学计算/符号计算/01.基本操作/角度弧度转化函数.py
@@ -0,0 +1,23 @@
+# 角度弧度转化函数
+
+from math import *
+print(
+    "01. 输入弧度是对的", 
+    sin(pi / 2), 
+)
+
+print(
+    "02. 如果输入的是角度, 要转成弧度",
+    sin(radians(90))
+)
+print(
+    "03. 弧度 ---> 角度", 
+    degrees(pi)
+)
+print(
+    "03. 角度 ---> 弧度, 就是 π", 
+    radians(180)
+)
+
+
+
